23d5560ba20172d4dc43a371f879c55505005ac2
qy <yiquwireless@163.com>
2013-07-28 20:23:51 +0800

    spinner save

--- src/com/yqwireless/exs/QueryFragment.java
+++ src/com/yqwireless/exs/QueryFragment.java
@@ -13,7 +13,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,31 +27,45 @@ import com.actionbarsherlock.app.SherlockFragment;
 import com.yqwireless.exs.EventCenter.EventListenr;
 
 public class QueryFragment extends SherlockFragment {
-	private TextView query_company, query_result;
+	private Spinner query_company;
+	private TextView  query_result;
 	private View query_query;
 	private EditText query_order;
+	String[] oftenUsedCompanys = {"查看所有", "全峰快递", "申通e物流", "EMS快递", "顺丰速运", "圆通速递", "中通快递",
+			"如风达快递", "韵达快运", "天天快递", "汇通快运",  "德邦物流", "宅急送" };
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		View v = inflater.inflate(R.layout.query_fragment, null);
-	    query_company = (TextView) v.findViewById(R.id.query_company);
+	    query_company = (Spinner) v.findViewById(R.id.query_company);
+	    
 		query_order = (EditText) v.findViewById(R.id.query_order);
 		query_query = v.findViewById(R.id.query_query);
 		query_result = (TextView) v.findViewById(R.id.query_result);
-		query_company.setOnClickListener(new OnClickListener() {
+		/*query_company.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View arg0) {
 				EventCenter center = EventCenter.getInstance();
 				center.onEvent("jump_company_select");
 			}
-		});
+		});*/
 		EventCenter.getInstance().regist(new EventListenr() {
 			
 			@Override
 			public void onEvent(Map<String, String> _data) {
 				String company = _data.get("company");
-				query_company.setText(company);
+				for (int i = 0; i < oftenUsedCompanys.length; i++) {
+					if (oftenUsedCompanys[i].equals(company)) {
+						query_company.setSelection(i);
+						query_company.setPrompt(company);
+						return;
+					}
+				}
+				oftenUsedCompanys[1] = company;
+				query_company.postInvalidate();
+				query_company.setSelection(1);
+				query_company.setPrompt(company);
 			}
 			
 			@Override
@@ -53,8 +73,45 @@ public class QueryFragment extends SherlockFragment {
 				return "company_select";
 			}
 		});
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+				getActivity(), android.R.layout.simple_list_item_1,
+				oftenUsedCompanys);
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		adapter.setNotifyOnChange(true);
+		query_company.setAdapter(adapter);
+		query_company.setSelection(4);
+		query_company.setOnItemSelectedListener(new OnItemSelectedListener() {
+
+			@Override
+			public void onItemSelected(AdapterView<?> arg0, View arg1,
+					int arg2, long arg3) {
+				System.out.println("onItemSelected " + arg2);
+				if (arg2 == 0) {
+					EventCenter center = EventCenter.getInstance();
+					center.onEvent("jump_company_select");
+				} else {
+					query_company.setPrompt(query_company.getAdapter().getItem(arg2).toString());
+				}
+				
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> arg0) {
+				System.out.println("onNothingSelected");
+					EventCenter center = EventCenter.getInstance();
+					center.onEvent("jump_company_select");
+			}
+		});
 		initQueryBtn();
 		return v;
+	}
+	
+	@Override
+	public void onResume() {
+		if (query_company.getSelectedItemPosition() == 0) {
+			query_company.setSelection(4);
+		}
+		super.onResume();
 	}
 	
 	private void initQueryBtn() {
@@ -69,7 +126,7 @@ public class QueryFragment extends SherlockFragment {
 				}
 				String orderNumber = query_order.getText().toString()
 						.trim();
-				String company_cn = query_company.getText().toString();
+				String company_cn = query_company.getPrompt().toString();
 				if (company_cn == null) {
 					Toast.makeText(getActivity(), R.string.company_empty,
 							Toast.LENGTH_SHORT).show();
---
+++ src/com/yqwireless/exs/QueryRecord.java
@@ -0,0 +1,7 @@
+package com.yqwireless.exs;
+
+public class QueryRecord {
+public String company_en;
+public String company_cn;
+public String last_query_result;
+}
